7ba94a999d43aa2043e74bd82873199b88553460
Shamoiev <wamoev@gmail.com>
2021-12-02 18:53:33 +0200

    Add yet another repositories

---
+++ src/main/java/com/wamoev/learningspring/data/entity/Reservation.java
@@ -0,0 +1,50 @@
+package com.wamoev.learningspring.data.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "RESERVATION_ID")
+    private long reservationId;
+    @Column(name = "ROOM_ID")
+    private long roomId;
+    @Column(name = "GUEST_ID")
+    private long guestId;
+    @Column(name = "RES_DATE")
+    private Date reservationDate;
+
+    public long getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(long reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(long roomId) {
+        this.roomId = roomId;
+    }
+
+    public long getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(long guestId) {
+        this.guestId = guestId;
+    }
+
+    public Date getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(Date reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+}
---
+++ src/main/java/com/wamoev/learningspring/data/repository/GuestRepository.java
@@ -0,0 +1,9 @@
+package com.wamoev.learningspring.data.repository;
+
+import com.wamoev.learningspring.data.entity.Guest;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends CrudRepository<Guest, Long> {
+}
