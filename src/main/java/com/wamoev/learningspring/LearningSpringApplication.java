6a87ed6bdca67815ea6c9df8d6dfa4f0a3b65632
Shamoiev <wamoev@gmail.com>
2021-12-02 15:40:45 +0200

    Create temp Rest controller for Rooms

--- src/main/java/com/wamoev/learningspring/LearningSpringApplication.java
+++ src/main/java/com/wamoev/learningspring/LearningSpringApplication.java
@@ -1,7 +1,13 @@
 package com.wamoev.learningspring;
 
+import com.wamoev.learningspring.data.entity.Room;
+import com.wamoev.learningspring.data.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearningSpringApplication {
@@ -10,4 +16,15 @@ public class LearningSpringApplication {
         SpringApplication.run(LearningSpringApplication.class, args);
     }
 
+    @RestController
+    @RequestMapping("/rooms")
+    public class RoomController {
+        @Autowired
+        private RoomRepository roomRepository;
+
+        @GetMapping
+        public Iterable<Room> getRooms() {
+            return roomRepository.findAll();
+        }
+    }
 }
