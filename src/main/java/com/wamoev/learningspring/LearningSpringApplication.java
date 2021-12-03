2bb5eb7a2b8b7a2296bc02eb130ef4b684ea92ae
Shamoiev <wamoev@gmail.com>
2021-12-03 12:49:16 +0200

    Clean application class

--- src/main/java/com/wamoev/learningspring/LearningSpringApplication.java
+++ src/main/java/com/wamoev/learningspring/LearningSpringApplication.java
@@ -1,58 +1,12 @@
 package com.wamoev.learningspring;
 
-import com.wamoev.learningspring.data.entity.Guest;
-import com.wamoev.learningspring.data.entity.Reservation;
-import com.wamoev.learningspring.data.entity.Room;
-import com.wamoev.learningspring.data.repository.GuestRepository;
-import com.wamoev.learningspring.data.repository.ReservationRepository;
-import com.wamoev.learningspring.data.repository.RoomRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearningSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearningSpringApplication.class, args);
-    }
-
-    @RestController
-    @RequestMapping("/rooms")
-    public class RoomController {
-        @Autowired
-        private RoomRepository roomRepository;
-
-        @GetMapping
-        public Iterable<Room> getRooms() {
-            return roomRepository.findAll();
-        }
-    }
-
-    @RestController
-    @RequestMapping("/guests")
-    public class GuestController {
-        @Autowired
-        private GuestRepository guestRepository;
-
-        @GetMapping
-        public Iterable<Guest> getRooms() {
-            return guestRepository.findAll();
-        }
-    }
-
-    @RestController
-    @RequestMapping("/reservations")
-    public class ReservationController {
-        @Autowired
-        private ReservationRepository reservationRepository;
-
-        @GetMapping
-        public Iterable<Reservation> getRooms() {
-            return reservationRepository.findAll();
-        }
     }
 }
