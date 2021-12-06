ed2dc0cc4a7d4a4bdefe631fc28f453eab744b1a
Shamoiev <wamoev@gmail.com>
2021-12-06 10:17:48 +0200

    Add REST controller

---
+++ src/main/java/com/wamoev/learningspring/web/RoomReservationWebServiceController.java
@@ -0,0 +1,29 @@
+package com.wamoev.learningspring.web;
+
+import com.wamoev.learningspring.business.domain.RoomReservation;
+import com.wamoev.learningspring.business.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/reservations")
+public class RoomReservationWebServiceController {
+    private final ReservationService reservationService;
+
+    @Autowired
+    public RoomReservationWebServiceController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping
+    public List<RoomReservation> getReservations(@RequestParam(name = "date", required = false) String dateString) {
+        Date date = DateUtils.createDateFromDateString(dateString);
+        return reservationService.getReservationForDate(date);
+    }
+}
